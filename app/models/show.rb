20ccadc59404ce8b3e9f38e3b7811fc9e647c2d1
Petya Djanovska <petya.djanovska@gmail.com>
2018-04-18 15:12:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.find_by rating: Show.maximum(:rating)
   end
+
+  def self.lowest_rating
+    Show.find_byrating: Show.minimum(:rating)
+  end
 end
